09fc9be35f041a92f8df1410fdf16a61b1d8d2f7
Nirjhor Nath <nirjhor.nath@gmail.com>
2021-12-08 21:46:54 -0500

    home page redirects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,37 @@
 import { Button, Container } from "@chakra-ui/react";
 import React from "react";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { getSession, signIn, signOut, useSession } from "next-auth/react";
+import { GetServerSideProps } from "next";
+import Head from "next/head";
 
 const Index = () => {
   const { data: session } = useSession();
 
   return (
-    <Container>
-      <Button
-        onClick={() =>
-          !session ? signIn("google", { callbackUrl: "/auth/osis" }) : signOut()
-        }
-      >
-        {session ? "Sign Out" : "Sign In"}
-      </Button>
-    </Container>
+    <>
+      <Head>
+        <title>TechCodes</title>
+      </Head>
+      <Container>
+        <Button
+          onClick={() =>
+            !session
+              ? signIn("google", { callbackUrl: "/auth/osis" })
+              : signOut()
+          }
+        >
+          {session ? "Sign Out" : "Sign In"}
+        </Button>
+      </Container>
+    </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+
+  if (session)
+    return { redirect: { destination: "/dashboard", permanent: false } };
 };
 
 export default Index;
